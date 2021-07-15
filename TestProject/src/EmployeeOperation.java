568f91e86d7d995439418b265dc00bf81c897a13
JashikaV <JashikaV@DSKBCP77.emids.local>
2021-07-15 19:43:00 +0530

    Second task

--- TestProject/src/EmployeeOperation.java
+++ TestProject/src/EmployeeOperation.java
@@ -2,10 +2,12 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class EmployeeOperation {
+	//first task
 	public static List<Employee> getEmployees() {
 		List<Employee> employees = Arrays.asList(new Employee("Jiya", "Brein", 32500.0, "HR"),
 				new Employee("Paul", "Niksui", 10500.0, "Sales And Marketing"),
@@ -38,37 +40,70 @@ public class EmployeeOperation {
 	}
 	
 	public static List<Employee> getEmployeeOnDepartment(String department) {
-		List<Employee> employees = EmployeeOperation.getEmployees();
-		return employees.stream().filter(x -> x.getDepartment().contains(department)).collect(Collectors.toList());
+		return EmployeeOperation.getEmployees().stream().filter(x -> x.getDepartment().contains(department)).collect(Collectors.toList());
 	}
 	
-	public static double getTotalSalary(){
-		List<Employee> employees = EmployeeOperation.getEmployees();
-		DoubleSummaryStatistics empSalary= employees.stream().collect(Collectors.summarizingDouble(Employee::getSalary));
+	public static double getTotalSalary() {
+		DoubleSummaryStatistics empSalary = EmployeeOperation.getEmployees().stream()
+				.collect(Collectors.summarizingDouble(Employee::getSalary));
 		return empSalary.getSum();
 	}
 	
 	public static Optional<Employee> getEmployeeWithHighestSalary() {
-		List<Employee> employees = EmployeeOperation.getEmployees();
-				 Optional<Employee> maxEmpSalary=employees.stream()
-			                .collect(Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary)));
-				return maxEmpSalary;
+		Optional<Employee> maxEmpSalary = EmployeeOperation.getEmployees().stream()
+				.collect(Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary)));
+		return maxEmpSalary;
 	}
 	
 	public static List<Employee> getEmployeesSortedOnDepartmentAndSalary() {
-		List<Employee> employees = EmployeeOperation.getEmployees();
-				return employees.stream().sorted(Comparator.comparing(Employee::getDepartment).thenComparing(Comparator.comparing(Employee::getSalary).reversed())).collect(Collectors.toList());
-				
+		return EmployeeOperation.getEmployees().stream()
+				.sorted(Comparator.comparing(Employee::getDepartment)
+						.thenComparing(Comparator.comparing(Employee::getSalary).reversed()))
+				.collect(Collectors.toList());
+
 	}
 	
+	//Second task
+	public static Map<String, List<Employee>> getEmployeesInDifferentForm() {
+		return EmployeeOperation.getEmployees().stream().collect(Collectors.groupingBy(Employee::getDepartment));
+	}
+
+	public static Map<String, Long> countTotalEmployeesPerDepartment() {
+		return EmployeeOperation.getEmployees().stream()
+				.collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+	}
+	
+	public static Map<String, Double> averageSalariesPerDepartment() {
+		return EmployeeOperation.getEmployees().stream().collect(
+				Collectors.groupingBy(Employee::getDepartment, Collectors.averagingDouble(Employee::getSalary)));
+	}
+	
+	public static List<Employee> getFirstFiveEmployees() {
+		return EmployeeOperation.getEmployees().stream().limit(5).collect(Collectors.toList());
+	}
+	
+	public static List<Employee> getSecondToFifthIndexEmployees() {
+		return EmployeeOperation.getEmployees().stream().skip(2).limit(4).collect(Collectors.toList());
+	}
 	
 	
 
 	public static void main(String[] args) {
 		System.out.println("List of Employees" + EmployeeOperation.getEmployees());
-		System.out.println("List of Employees based on department" + EmployeeOperation.getEmployeeOnDepartment(Constants.FINANCE_DEPARTMENT));
+		System.out.println("List of Employees based on department"
+				+ EmployeeOperation.getEmployeeOnDepartment(Constants.FINANCE_DEPARTMENT));
 		System.out.println("Total salary " + EmployeeOperation.getTotalSalary());
 		System.out.println("Employee with max salary is " + EmployeeOperation.getEmployeeWithHighestSalary());
-		System.out.println("Employees after sorting by department and decreasing order of salary" + EmployeeOperation.getEmployeesSortedOnDepartmentAndSalary());
-}
+		System.out.println("Employees after sorting by department and decreasing order of salary"
+				+ EmployeeOperation.getEmployeesSortedOnDepartmentAndSalary());
+		System.out.println("List of employees based on department in map structure"
+				+ EmployeeOperation.getEmployeesInDifferentForm());
+		System.out
+				.println("Count total employees per department" + EmployeeOperation.countTotalEmployeesPerDepartment());
+		System.out.println(
+				"Average salaries of employees per department" + EmployeeOperation.averageSalariesPerDepartment());
+		System.out.println("Get first five employees" + EmployeeOperation.getFirstFiveEmployees());
+		System.out.println(
+				"Get employess from 2nd index to fifth index" + EmployeeOperation.getSecondToFifthIndexEmployees());
+	}
 }
